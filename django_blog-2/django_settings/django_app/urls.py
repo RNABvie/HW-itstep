501b661e54747239d0a2a8717f5775169271cf6a
RNABvie <134534535+RNABvie@users.noreply.github.com>
2023-09-17 13:34:15 +0300

    Delete django_blog-2/django_settings directory

--- django_blog-2/django_settings/django_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, reverse, reverse_lazy
-from django_app import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path('registrations/', views.register, name="register"),
-    path("login/", views.login_, name="login"),
-]
